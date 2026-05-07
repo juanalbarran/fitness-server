51f3df3f6123f30b68b323c13d75e2d9c3bb1d60
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-05-07 17:59:16 +0200

    User
    Added User fields, constructor and Object methods

--- src/main/java/com/jarsolutions/fitness/user/domain/entity/User.java
+++ src/main/java/com/jarsolutions/fitness/user/domain/entity/User.java
@@ -1,3 +1,75 @@
 package com.jarsolutions.fitness.user.domain.entity;
 
-public class User {}
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user_account")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true)
+  private String username;
+
+  @Column(nullable = true)
+  private String password;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  public User(String username, String password, String email) {
+    this.username = username;
+    this.password = password;
+    this.email = email;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof User)) return false;
+    return id != null && id.equals(((User) o).getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
