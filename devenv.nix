106724b7099c49264d743e81754cb71626f80ef1
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-05-07 17:24:06 +0200

    Skeleton
    Adding skeleton of the application

---
+++ devenv.nix
@@ -0,0 +1,56 @@
+# java/devenv.nix
+{pkgs, ...}: let
+  application_name = "fitness-server";
+  database_name = "fitness";
+  database_username = "fitness";
+  database_password = "fitness";
+  database_host = "127.0.0.1";
+  database_port = 5432;
+  allowed-origins = "http://localhost:3000";
+in {
+  # https://devenv.sh/basics/
+  env.GREET = "Java & Spring Boot Development";
+  env.application_name = application_name;
+  env.database_name = database_name;
+  env.database_username = database_username;
+  env.database_password = database_password;
+  env.database_host = database_host;
+  env.database_port = database_port;
+
+  env.allowed-origins = allowed-origins;
+
+  packages = with pkgs; [
+    maven
+    spring-boot-cli
+    pgcli
+    liquibase
+  ];
+
+  # Enable Java (Temurin 21 is the current standard for modern Spring Boot)
+  languages.java = {
+    enable = true;
+    jdk.package = pkgs.temurin-bin-21;
+  };
+
+  # Local PostgreSQL 18 configuration
+  services.postgres = {
+    enable = true;
+    package = pkgs.postgresql_18;
+    initialDatabases = [{name = database_name;}];
+    listen_addresses = database_host;
+    port = database_port;
+    initialScript = ''
+      CREATE USER ${database_username} WITH PASSWORD '${database_password}' SUPERUSER;
+      ALTER DATABASE ${database_name} OWNER TO ${database_username};
+    '';
+  };
+
+  enterShell = ''
+    echo "--- $GREET ---"
+    java -version
+    mvn -version
+    echo "------------------------------------------------"
+    echo "DB: postgresql://${database_host}:${toString database_port}/${database_name}"
+    echo "------------------------------------------------"
+  '';
+}
---
+++ src/main/java/com/jarsolutions/fitness/FitnessApplication.java
@@ -0,0 +1,13 @@
+package com.jarsolutions.authentication_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessApplication.class, args);
+	}
+
+}
