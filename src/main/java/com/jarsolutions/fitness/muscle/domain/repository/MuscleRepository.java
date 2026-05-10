960b8b9fb53f991b986bfc9f4eb12e9b154443fc
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-05-10 21:55:22 +0200

    #2 MuscleGroup: Starting Hexagonal architecture
    I decided to try the hexagonal architecture, lot's of code

--- src/main/java/com/jarsolutions/fitness/muscle/domain/repository/MuscleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jarsolutions.fitness.muscle.domain.repository;
-
-import com.jarsolutions.fitness.muscle.domain.entity.Muscle;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MuscleRepository extends JpaRepository<Muscle, Long> {}
--- src/main/java/com/jarsolutions/fitness/musclegroup/domain/repository/MuscleGroupRepository.java
+++
@@ -1,6 +0,0 @@
-package com.jarsolutions.fitness.musclegroup.domain.repository;
-
-import com.jarsolutions.fitness.musclegroup.domain.entity.MuscleGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MuscleGroupRepository extends JpaRepository<MuscleGroup, Long> {}
